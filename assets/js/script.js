6b1ef9914f3b54cc31900b93e1bdce6e1dbc27b8
Haram Kwon <kharam1436@gmail.com>
2021-02-01 23:50:21 -0800

    async, and mapping, and getting multiple days code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 "use strict";
 
+// Required place for openweather api
 const ApiURL = "https://api.openweathermap.org/data/2.5";
 const currentWeatherApiURL = `${ApiURL}/weather`;
-const dailyWeatherApiURL = `${ApiURL}/daily`;
+const dailyWeatherApiURL = `${ApiURL}/forecast/daily`;
 
-const apiKey = "b003fb9dacba939b22f1106e25ad5a19";
-const city = "seattle";
+// Temporary place holder for api key, and city
+const APIKEY = "b003fb9dacba939b22f1106e25ad5a19";
 
-async function getCurrentWeather() {
-  const requestUrl = `${currentWeatherApiURL}?q=${city}&appid=${apiKey}`;
+async function getCurrentWeather(city) {
+  const requestUrl = makeQueryURLWithKey(currentWeatherApiURL, {
+    q: `${city}`,
+  });
 
   await fetch(requestUrl)
     .then((response) => response.json())
@@ -17,26 +20,38 @@ async function getCurrentWeather() {
     });
 }
 
-async function getNextFiveDaysForecast() {}
+async function getNextFiveDaysForecast(city, cnt) {
+  const requestUrl = makeQueryURLWithKey(dailyWeatherApiURL, {
+    q: `${city}`,
+    cnt: `${cnt}`,
+  });
 
-function makeQueryURLWithKey(baseURL, queryObject) {
+  console.log(requestUrl);
+  await fetch(requestUrl)
+    .then((response) => response.json)
+    .then((data) => console.log(data));
+}
+
+/**
+ * Making a qeury with base URL, and parameter. (Need to give api key)
+ *
+ * @param String baseURL
+ * @param Object queryObject
+ * @param String apiKey
+ */
+const makeQueryURLWithKey = function (baseURL, queryObject, apiKey = APIKEY) {
+  function makeQueryURL(baseURL, queryObject) {
+    // join the query into the string
+    const query = Object.keys(queryObject)
+      .map((k) => `${escape(k)}=${escape(queryObject[k])}`)
+      .join("&");
+
+    return `${baseURL}?${query}`;
+  }
+
   queryObject["appid"] = apiKey;
   return makeQueryURL(baseURL, queryObject);
-}
+};
 
-function makeQueryURL(baseURL, queryObject) {
-  // join the query into the string
-  const query = Object.keys(queryObject)
-    .map((k) => `${escape(k)}=${escape(queryObject[k])}`)
-    .join("&");
-
-  return `${baseURL}?${query}`;
-}
-
-// getCurrentWeather();
-const temp = makeQueryURLWithKey(currentWeatherApiURL, {
-  name: "hi",
-  kwon: "good",
-});
-
-console.log(temp);
+// getCurrentWeather("seattle");
+getNextFiveDaysForecast("seattle", 7);
